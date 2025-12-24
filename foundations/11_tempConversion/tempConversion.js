6b14904df777dc3265b10fed87ac5a2027d342da
Tim Copley <249600077+timcopley@users.noreply.github.com>
2025-12-24 16:21:50 -0500

    Completed exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  return +((tempInF - 32) / 1.8).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+    return +(tempInC * 1.8 + 32).toFixed(1);
 };
 
 // Do not edit below this line
