73b6b90cd0b1dc724efb0e9848962eccc89c1539
Tim Copley <249600077+timcopley@users.noreply.github.com>
2025-12-24 15:51:58 -0500

    Completed exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) {
 
+    function verifyNum(num) {
+        if (num >= 0 && Math.floor(num) === num && typeof(num) === 'number') {
+            return true
+        } else return false
+    }
+
+    if (!verifyNum(numA) || !verifyNum(numB)) return 'ERROR';
+
+    let sum = 0;
+    let a, b;
+    if (numA <= numB) {
+        a = numA;
+        b = numB;
+    } else { 
+        a = numB;
+        b = numA;
+    }
+
+    for (let i = a; i <= b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
