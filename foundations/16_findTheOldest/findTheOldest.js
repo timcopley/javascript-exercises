d19d4efa216574a2c92d021a1e003cabbc55c282
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-12 11:47:16 -0500

    Completed exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const personAge = (person.yearOfDeath || 2026) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || 2026) - oldest.yearOfBirth;
+        if (personAge > oldestAge) {
+            oldest = person;
+        }
+        return oldest
+    }, people[0]);
 };
 
 // Do not edit below this line
