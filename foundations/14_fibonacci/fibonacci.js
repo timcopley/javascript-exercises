90f1e7301861e8ef9d8c13f5b834a280d6a6e849
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-12 11:28:20 -0500

    Completed exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
+    if (index === 0) return 0;
+    if (index < 0) return 'OOPS';
+    
+    let prev1 = 0;
+    let prev2 = 1;
+    let curr = 1;
 
+    for (let i = 1; i < index; i++) {
+        curr = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = curr;
+    }
+
+    return curr;
 };
 
 // Do not edit below this line
