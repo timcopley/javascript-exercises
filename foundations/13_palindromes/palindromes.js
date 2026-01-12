9db8c90364a3a146628092f3948d13a6fd403faa
Tim Copley <249600077+timcopley@users.noreply.github.com>
2026-01-12 11:06:59 -0500

    Completed exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    // strip punctuation
+    const strippedText = text.replace(/[^A-Za-z0-9]/g, '');
+    const lowerText = strippedText.toLowerCase();
+    const reversedText = lowerText.split('').reverse().join('');
 
+    return lowerText === reversedText;
 };
 
 // Do not edit below this line
